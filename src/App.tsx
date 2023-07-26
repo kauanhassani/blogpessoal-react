1b9063e83390ae0377c3a359cd1c6ebfb069e6d2
kauanhassani <kauanhassani@gmail.com>
2023-07-26 13:33:46 -0300

    Inserção Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import Home from './paginas/home/Home';
-import './App.css';
 import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
 
 function App() {
- 
   return (
     <>
-      <Home/>
+    <Home/>
     </>
-  );
+);
 }
-
 export default App;
